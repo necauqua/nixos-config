06670eb036022fc8b933932deef52b628d52b86f
Anton Bulakh <him@necauq.ua>
2023-11-20 00:27:37 +0200

    Enable nix flakes and stuff for the occasional ssh

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -6,6 +6,7 @@
 
   nix = {
     nixPath = [ "nixpkgs=${flakeInputs.nixpkgs}" ];
+    extraOptions = "experimental-features = nix-command flakes repl-flake";
     registry.nixos = {
       from = { id = "nixos"; type = "indirect"; };
       flake = flakeInputs.nixpkgs;
