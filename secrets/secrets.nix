42def333e41c9f8366117eef4dddb04d1a16efdc
Anton Bulakh <him@necauq.ua>
2023-11-22 01:18:35 +0200

    Migrate the maven repo

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
-  necauqua = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0oajjYx0nt7A2zBWjnc5gxTs1nBcGHuGNyp0Al5rAz openpgp:0xA61191F9";
+  him = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG+3CzNhhWDQppHT1of+a8QCzlgQidlgUWEBMYUJnrfk";
   offsite = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIATmTop5IhHPpDQUS5l/HA+LgvLtecby0+97Pu/+PI+P";
 in
 {
-  "murmur-password.age".publicKeys = [ necauqua offsite ];
+  "murmur-password.age".publicKeys = [ him offsite ];
+  "reposilite-password.age".publicKeys = [ him offsite ];
 }
