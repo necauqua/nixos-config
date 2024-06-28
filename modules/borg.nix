1943326c4f1ee6f178fe6b3f62644148ec1beb65
Anton Bulakh <him@necauq.ua>
2024-06-28 17:32:32 +0300

    Fix backup being broken in a couple of ways

--- modules/borg.nix
+++ modules/borg.nix
@@ -2,13 +2,16 @@
   services.borgbackup.jobs.offsite = {
     archiveBaseName = "home";
     dateFormat = "+1%Y-%m-%dT%H:%M:%S";
-    paths = [ "/home/necauqua" ];
     repo = "borg@necauq.ua:.";
     encryption.mode = "repokey-blake2";
     encryption.passCommand = "cat /run/keys/borgbackup_passphrase";
     environment.BORG_RSH = "ssh -i /root/.ssh/borg_ed25519 -p 5555";
 
-    extraCreateArgs = [ "--exclude-caches" ];
+    preHook = ''
+      extraCreateArgs=--exclude-caches # nix-side extraCreateArgs are broken atm
+      cd /home/necauqua # ugh patterns are relative to cwd
+    '';
+    paths = [ "." ];
 
     startAt = "daily";
     prune.keep = {
