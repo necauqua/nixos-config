6e563cc7df83115e47a9628d4dd8aef22c75bc2e
Anton Bulakh <him@necauq.ua>
2024-06-28 17:39:06 +0300

    Add the phone SSH pk

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,6 +1,7 @@
 {
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL0oajjYx0nt7A2zBWjnc5gxTs1nBcGHuGNyp0Al5rAz openpgp:0xA61191F9"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJoNFwj1SN1LJGT6Pto7hp9kHhWF9RsF0tXMI95Jix5P phone"
   ];
   services.openssh = {
     enable = true;
@@ -12,6 +13,6 @@
     enable = true;
     port = 22;
     openFirewall = true;
-    extraOptions = ["-vd" "999999"];
+    extraOptions = [ "-vd" "999999" ];
   };
 }
