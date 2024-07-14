9e81c56710e5e19b53c3b66772509a2a87ba6acc
Anton Bulakh <him@necauq.ua>
2024-07-14 16:00:53 +0300

    flex: Change nvidia prime thing to not use the GPU for desktop

--- machines/flex.nix
+++ machines/flex.nix
@@ -28,10 +28,10 @@
         in
         ''
           ${xrandr} --newmode "1080x2160" 200.61 1080 1168 1288 1496 2160 2161 2164 2235 -hsync +vsync
-          ${xrandr} --addmode HDMI-1-1 1080x2160
+          ${xrandr} --addmode HDMI-1 1080x2160
           echo on > /sys/kernel/debug/dri/1/HDMI-A-1/force
           echo off > /sys/kernel/debug/dri/1/HDMI-A-1/force
-          ${pkgs.xorg.xinput}/bin/xinput --map-to-output "pointer:ELAN9008:00 04F3:2A46" eDP-1-1
+          ${pkgs.xorg.xinput}/bin/xinput --map-to-output "pointer:ELAN9008:00 04F3:2A46" eDP-1
           ${pkgs.unclutter-xfixes}/bin/unclutter --hide-on-touch -b
         '';
       screenSection = ''
@@ -44,7 +44,7 @@
 
   # do the nvidia+intel laptop magic
   hardware.nvidia.prime = {
-    sync.enable = true;
+    offload.enable = true;
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:2:0:0";
   };
