17f8d216cacca166e65692a8b319413757457224
Anton Bulakh <him@necauq.ua>
2024-07-23 17:55:57 +0300

    Resurrect picom too

---
+++ home/profiles/picom.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }: {
+  services.picom = {
+    enable = true;
+    package = pkgs.picom-next;
+    settings = {
+      backend = "glx";
+      blur-background = true;
+      blur-background-exclude = [
+        "window_type = 'dock'"
+        "window_type = 'desktop'"
+        "class_g = 'Peek'"
+        "class_g = 'slop'"
+        "class_g = 'firefox'" # firefox menus
+        "class_g = 'TelegramDesktop'" # telegram menus too
+      ];
+      blur = {
+        method = "dual_kawase";
+        strength = 4;
+      };
+      dbus = true;
+      unredir-if-possible = true;
+    };
+  };
+}
