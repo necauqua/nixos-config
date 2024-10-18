95ac22c78524e8614dde84aea93052e4cc6e67df
Anton Bulakh <him@necauq.ua>
2024-10-18 05:01:24 +0300

    Switch to bleeding-edge hyprland for the ghidra hang fix

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -18,7 +18,36 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
+
+    package = pkgs.hyprland.overrideAttrs (prev: {
+      src = pkgs.fetchFromGitHub {
+        owner = "hyprwm";
+        repo = "hyprland";
+        fetchSubmodules = true;
+        rev = "0e630e9e74ad34683194a07cfe6afe55a2c0685f";
+        hash = "sha256-nTMzcwH5eFX2JM5Lrtw1469BRe6hGgWWxLqJBynEdvo=";
+      };
+    });
   };
+
+  # omg lol
+  nixpkgs.overlays = [
+    (final: prev: {
+      aquamarine = prev.aquamarine.overrideAttrs (prev: {
+        src = final.fetchFromGitHub {
+          owner = "hyprwm";
+          repo = "aquamarine";
+          rev = "v0.4.3";
+          hash = "sha256-44bnoY0nAvbBQ/lVjmn511yL39Sv7SknV0BDxn34P3Q=";
+        };
+      });
+    })
+  ];
+
+  assertions = [{
+    assertion = pkgs.hyprland.version == "0.44.1";
+    message = "Check if then-bleeding hyprland is still needed";
+  }];
 
   # electron
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
