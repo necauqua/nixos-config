c368baac9891e6b0b44dc284885c4695cba362f1
Anton Bulakh <him@necauq.ua>
2024-12-08 00:19:10 +0200

    safe-rm: also log the removals

--- home/profiles/safe-rm.nix
+++ home/profiles/safe-rm.nix
@@ -4,12 +4,18 @@
     default = true;
   };
   config = {
-    home.packages = with pkgs; [
+    home.packages = with pkgs; lib.optionals config.safe-rm [
       httm
       zfs-prune-snapshots
     ];
     programs.fish.functions.rm = lib.mkIf config.safe-rm {
-      body = "${pkgs.httm}/bin/ounce --suffix pre-rm rm $argv";
+      body = ''
+        ${pkgs.httm}/bin/ounce --suffix pre-rm rm $argv
+        set -l st $status
+        set -l snap $(zfs list -t snapshot -o name -HS creation | head -1)
+        echo -e "rm $argv | ref $snap" >> ~/.rm-history
+        return $st
+      '';
       wraps = "rm";
     };
   };
