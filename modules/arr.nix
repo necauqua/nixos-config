baaa1be8c803da21580feb62148a80bda14ea44d
Anton Bulakh <him@necauq.ua>
2024-12-21 14:39:50 +0200

    post-update: permit some now-insecure packages for sonarr

--- modules/arr.nix
+++ modules/arr.nix
@@ -1,4 +1,11 @@
 {
+  # at least sonarr depends on those for now
+  nixpkgs.config.permittedInsecurePackages = [
+    "aspnetcore-runtime-6.0.36"
+    "aspnetcore-runtime-wrapped-6.0.36"
+    "dotnet-sdk-6.0.428"
+    "dotnet-sdk-wrapped-6.0.428"
+  ];
   services =
     let yup = { enable = true; openFirewall = true; };
     in {
