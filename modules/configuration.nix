c574e35af55264ca8d451be42d7243c030a60b2f
Anton Bulakh <him@necauq.ua>
2024-12-26 01:21:27 +0200

    less: update default flags
    Fix the mouse issue finally, also -X seems bad?.
    Why did the very past me enable it, I got no clue.

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -133,7 +133,7 @@
   programs = {
     adb.enable = true;
     fish.enable = true;
-    less.envVariables.LESS = "-FRX";
+    less.envVariables.LESS = "-FR --mouse";
     partition-manager.enable = true;
     steam.enable = true;
     wireshark.enable = true;
