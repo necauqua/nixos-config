993c7969a61da1dcb5e95338421533c3e56c5dac
Anton Bulakh <him@necauq.ua>
2026-06-02 19:45:24 +0300

    fix(nvidia): enable nvidia-container-toolkit of podman is enabled too

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -11,5 +11,8 @@
   # huh
   environment.variables.LIBVA_DRIVER_NAME = lib.mkDefault "nvidia";
 
-  hardware.nvidia-container-toolkit.enable = config.virtualisation.docker.enable;
+  hardware.nvidia-container-toolkit.enable = config.virtualisation.docker.enable || config.virtualisation.podman.enable;
+
+  # seems like the above enable actually does nothing without this, upstream oopsie?
+  virtualisation.docker.enableNvidia = config.virtualisation.docker.enable || config.virtualisation.podman.enable;
 }
